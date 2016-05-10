9aa9c37378187940e69b843d4722b6fd33dc1658
Daniel Beilinson <beilinson@gmail.com>
2016-05-10 23:36:14 +0300

    Seed entities table.

---
+++ db/seed/entitiesSeed.sql
@@ -0,0 +1,7 @@
+-- seed entities:
+
+insert into entities(entity_id, title, created, edited, author, class, data)
+values ('1', 'Martin Luther King, Jr', NOW(), NOW(), 'testuser', 'Person', '{"firstname": "Martin Luther", "lastname": "King", "occupation": ["Baptist minister", "social activist"], "organization": "African-American Civil Rights Movement"}'),
+('2', 'Jim Morrison', NOW(), NOW(), 'testuser', 'Person', '{"firstname": "Jim", "lastname": "Morisson", "occupation": ["musician", "poet"], "organization": "The Doors"}'),
+('3', 'Amnesty International', NOW(), NOW(), 'testuser2', 'Organization', '{"type": "NGO", "founded": "1961", "location": "London"}'),
+('4', 'Berlin Wall collapse', NOW(), NOW(), 'testuser2', 'Event', '{"date": "1989-11-09T21:00:00.000Z", "location": "Berlin"}');
