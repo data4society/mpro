48ad0f1971c1e457c7296b98d6cb03e6db5b2fb8
Daniel Beilinson <beilinson@gmail.com>
2016-05-11 14:07:52 +0300

    Create seed script.

---
+++ seed.js
@@ -0,0 +1,28 @@
+var DocumentStore = require('./server/DocumentStore');
+var EntityStore = require('./server/EntityStore');
+var SessionStore = require('./server/SessionStore');
+var ThematicStore = require('./server/ThematicStore');
+var UserStore = require('./server/UserStore');
+var Database = require('./server/Database');
+var db = new Database();
+
+db.reset() // Clear the database, set up the schema
+  .then(function() {
+    var userStore = new UserStore({ db: db });
+    return userStore.seed();
+  }).then(function() {
+    var sessionStore = new SessionStore({ db: db });
+    return sessionStore.seed();
+  }).then(function() {
+    var entityStore = new EntityStore({ db: db });
+    return entityStore.seed();
+  }).then(function() {
+    var thematicStore = new ThematicStore({ db: db });
+    return thematicStore.seed();
+  }).then(function() {
+  var documentStore = new DocumentStore({ db: db });
+    return documentStore.seed();
+  }).then(function() {
+    console.log('Done seeding.');
+    db.connection.end();
+  });
