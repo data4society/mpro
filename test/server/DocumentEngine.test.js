b4adef0f312580a0af0043c30f78a7596a37db47
Daniel Beilinson <beilinson@gmail.com>
2016-05-20 20:49:39 +0300

    More tests for Document Engine.

--- test/server/DocumentEngine.test.js
+++ test/server/DocumentEngine.test.js
@@ -90,6 +90,7 @@ describe('Document Engine', function() {
         doc.should.be.an('object');
         assert.equal(doc.documentId, args.documentId);
         should.exist(doc.data);
+        assert.equal(doc.data.schema.name, args.schemaName);
         done();
       });
     });
@@ -104,6 +105,95 @@ describe('Document Engine', function() {
         doc.should.be.an('object');
         should.exist(doc.data);
         should.exist(doc.documentId);
+        done();
+      });
+    });
+  });
+
+  describe('Create change', function() {
+    it('should not allow adding a change to non existing document', function(done) {
+      var args = {
+        documentId: 'some-non-existent-doc',
+        change: {'some': 'change'}
+      };
+
+      documentEngine.addChange(args, function(err, version) {
+        should.exist(err);
+        should.not.exist(version);
+        done();
+      });
+    });
+
+    it('should add a new change to existing document', function(done) {
+      var args = {
+        documentId: '1',
+        change: {'some': 'change'}
+      };
+
+      documentEngine.addChange(args, function(err, version) {
+        should.not.exist(err);
+        should.exist(version);
+        version.should.be.a('number');
+        assert.equal(version, 5);
+
+        var args = {
+          documentId: '1',
+          sinceVersion: 0
+        };
+
+        documentEngine.getChanges(args, function(err, result) {
+          assert.equal(result.changes.length, 5);
+          assert.equal(result.version, 5);
+
+          documentEngine.documentStore.getDocument('1', function(err, doc) {
+            assert.equal(doc.version, 5);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('Read document', function() {
+    var documentId = '1';
+
+    it('document should be valid', function(done) {
+      documentEngine.getDocument({documentId: documentId}, function(err, doc) {
+        should.not.exist(err);
+        doc.should.be.an('object');
+        should.exist(doc.data);
+        should.exist(doc.documentId);
+        assert.equal(doc.version, 1);
+        done();
+      });
+    });
+
+    it('should return right version', function(done) {
+      documentEngine.getVersion('1', function(err, version) {
+        should.not.exist(err);
+        should.exist(version);
+        version.should.be.a('number');
+        assert.equal(version, 4);
+        done();
+      });
+    });
+  });
+
+  describe('Read changes', function() {
+    var documentId = '1';
+
+    it('changes should be valid', function(done) {
+      var args = {
+        documentId: '1',
+        sinceVersion: 0
+      };
+
+      documentEngine.getChanges(args, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.should.be.an('object');
+        assert.equal(result.changes.length, 4);
+        assert.equal(result.version, 4);
         done();
       });
     });
@@ -142,99 +232,6 @@ describe('Document Engine', function() {
   });
 });
 
-// QUnit.test("Test if seed db has a valid document test-doc", function(assert) {
-//   var done = assert.async();
-//   documentEngine.getDocument({documentId: 'test-doc'}, function(err, doc) {
-//     assert.ok(doc, 'valid doc snapshot expected');
-//     assert.ok(doc.data, 'should have document data attached');
-//     assert.equal(doc.version, 1, 'doc version should be 1');
-//     done();
-//   });
-// });
-
-// QUnit.test('Create a new document', function(assert) {
-//   var done = assert.async();
-//   documentEngine.createDocument({
-//     documentId: 'new-doc',
-//     schemaName: 'prose-article'
-//   }, function(err, doc) {
-//     assert.ok(doc.data, 'valid doc snapshot expected');
-//     done();
-//   });
-// });
-
-// QUnit.test('Create a new document without documentId provided', function(assert) {
-//   var done = assert.async();
-//   documentEngine.createDocument({
-//     schemaName: 'prose-article'
-//   }, function(err, doc) {
-//     assert.ok(doc.data, 'valid doc snapshot expected');
-//     assert.ok(doc.documentId, 'Auto-generated documentId expected');
-//     done();
-//   });
-// });
-
-// QUnit.test('Delete document', function(assert) {
-//   var done = assert.async();
-//   documentEngine.deleteDocument('test-doc', function(err) {
-//     assert.ok(!err, 'Should delete a document');
-//     documentEngine.getDocument('test-doc', function(err, doc) {
-//       assert.ok(err, 'Should print an error that document does not exist');
-//       assert.isNullOrUndefined(doc, 'doc should be undefined');
-
-//       // Test if there are still changes for that doc after deletion
-//       var args = {
-//         documentId: 'test-doc',
-//         sinceVersion: 0
-//       };
-//       documentEngine.getChanges(args, function(err) {
-//         assert.ok(err, 'Should print an error that document does not exist');
-//       });
-//       documentEngine.changeStore.getChanges(args, function(err, result) {
-//         assert.equal(result.changes.length, 0, 'Should be no changes');
-//         assert.equal(result.version, 0, 'Document version should equals 0');
-//         done();
-//       });
-//     });
-//   });
-// });
-
-// // Changes Store API (Required by CollabEngine)
-// // --------------------
-
-// QUnit.test("Test if seed db test-doc has the right version", function(assert) {
-//   var done = assert.async();
-//   documentEngine.getVersion('test-doc', function(err, version) {
-//     assert.notOk(err, 'Should not error');
-//     assert.equal(version, 1, 'Document version should equals 1');
-//     done();
-//   });
-// });
-
-// QUnit.test("Test if seed db test-doc has valid changes", function(assert) {
-//   var done = assert.async();
-//   var args = {
-//     documentId: 'test-doc',
-//     sinceVersion: 0
-//   };
-//   documentEngine.getChanges(args, function(err, result) {
-//     assert.notOk(err, 'Should not error');
-//     assert.equal(result.changes.length, 1, 'Should be only one change');
-//     assert.equal(result.version, 1, 'Document version should equals 1');
-//     done();
-//   });
-// });
-
-// QUnit.test('Should not allow adding a change to non existing changeset', function(assert) {
-//   var done = assert.async();
-//   documentEngine.addChange({
-//     documentId: 'some-non-existent-doc',
-//     change: {'some': 'change'}
-//   }, function(err) {
-//     assert.ok(err, 'Adding change to non existent doc should error');
-//     done();
-//   });
-// });
 
 // QUnit.test('Add a change to an existing doc', function(assert) {
 //   var done = assert.async();
