d8986d67716d4f888026bbb2af3750e68c1e1267
Daniel Beilinson <beilinson@gmail.com>
2016-07-12 21:27:30 +0300

    Connect filters.

--- packages/inbox/Inbox.js
+++ packages/inbox/Inbox.js
@@ -2,8 +2,9 @@
 
 var extend = require('lodash/extend');
 var Component = require('substance/ui/Component');
-var ScrollPane = require('substance/ui/ScrollPane');
+var ListScrollPane = require('../common/ListScrollPane');
 var DoubleSplitPane = require('../common/DoubleSplitPane');
+var AbstractFeedLoader = require('../common/AbstractFeedLoader');
 
 /*
   Represents Inbox page.
@@ -14,7 +15,11 @@ var DoubleSplitPane = require('../common/DoubleSplitPane');
   - Document Viewer
 */
 function Inbox() {
-  Component.apply(this, arguments);
+  AbstractFeedLoader.apply(this, arguments);
+
+  this.handleActions({
+    'loadMore': this._loadMore
+  });
 }
 
 Inbox.Prototype = function() {
@@ -23,6 +28,7 @@ Inbox.Prototype = function() {
     var componentRegistry = this.context.componentRegistry;
     var Header = componentRegistry.get('header');
     var Feed = componentRegistry.get('feed');
+    var Filters = componentRegistry.get('filters');
 
     var el = $$('div').addClass('sc-inbox');
 
@@ -34,12 +40,12 @@ Inbox.Prototype = function() {
 
     el.append(
       $$(DoubleSplitPane, {splitType: 'vertical', sizeA: '300px', sizeB: '40%'}).append(
-        $$('div').addClass('test1'),
-        $$(ScrollPane, {
+        $$(Filters, {rubrics: this.state.rubrics}).ref('filters'),
+        $$(ListScrollPane, {
           scrollbarType: 'substance',
           scrollbarPosition: 'left'
         }).append(
-          $$(Feed, extend({}, this.props, {rubrics: ''})).ref('feed')
+          $$(Feed, extend({}, this.props, this.state)).ref('feed')
         ),
         $$('div').addClass('test3')
       )
@@ -48,8 +54,25 @@ Inbox.Prototype = function() {
     return el;
   };
 
+  this._loadMore = function(page) {
+    this.extendState({
+      page: page,
+      pagination: true
+    });
+    this._loadDocuments();
+  };
+
+  this._applyFacets = function(facets) {
+    var filters = this.state.filters;
+    filters["rubrics @>"] = facets;
+    this.extendState({
+      filters: filters
+    });
+    this._loadDocuments();
+  };
+
 };
 
-Component.extend(Inbox);
+AbstractFeedLoader.extend(Inbox);
 
 module.exports = Inbox;
