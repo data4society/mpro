e219496ad169017a40f4ef6e03f6923d21bd1956
Daniel Beilinson <beilinson@gmail.com>
2016-07-12 21:24:24 +0300

    Facets package.

---
+++ packages/facets/Facets.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var map = require('lodash/map');
+var each = require('lodash/each');
+var keyBy = require('lodash/keyBy');
+var concat = require('lodash/concat');
+var isEqual = require('lodash/isEqual');
+var flattenDeep = require('lodash/flattenDeep');
+var isUndefined = require('lodash/isUndefined');
+var isEmpty = require('lodash/isEmpty');
+var Component = require('substance/ui/Component');
+var Icon = require('substance/ui/FontAwesomeIcon');
+
+function Facets() {
+  Component.apply(this, arguments);
+}
+
+Facets.Prototype = function() {
+
+  this.render = function($$) {
+    var el = $$('div').addClass('sc-facets');
+    var rubrics = this.props.rubrics;
+    if(!isEmpty(rubrics)) {
+      var childNodes = rubrics.getRoots();
+
+      var childEls = childNodes.map(function(node) {
+        return this.renderChildren($$, node, 1);
+      }.bind(this));
+      
+      el.append(flattenDeep(childEls));
+    }
+
+    return el;
+  };
+
+  this.renderChildren = function($$, node, level) {
+    var rubrics = this.props.rubrics;
+    var isSelected = node.active;
+    var hasSelectedChildren = rubrics.hasActiveChildren(node.id);
+    var isExpanded = node.expanded || isSelected || hasSelectedChildren;
+    var childNodes = rubrics.getChildren(node.id);
+    var hideExpand = isEmpty(childNodes);
+    var childrenEls = [];
+
+    if(level === 1) {
+      isExpanded = true;
+      hideExpand = true;
+    }
+
+    if(isExpanded) {
+      childrenEls = map(childNodes, function(сhildNode) {
+        return this.renderChildren($$, сhildNode, level + 1);
+      }.bind(this));
+    }
+
+    var el = $$('div').addClass('se-tree-node').ref(node.id);
+
+    if(isSelected) el.addClass('active');
+
+    // level graphical nesting
+    if(hideExpand && level !== 1) {
+      level = level * 2;
+      if(level == 4) level = 5;
+    }
+    var levelSign = new Array(level).join('·') + ' ';
+    el.append(levelSign);
+
+    if(!hideExpand) {
+      var expandedIcon = isExpanded ? 'expanded' : 'collapsed';
+      el.append(
+        this.context.iconProvider.renderIcon($$, expandedIcon).addClass('expansion')
+          .on('click', this._expandNode.bind(this, node.id))
+      );
+    }
+
+    if(level === 1) {
+      el.addClass('se-tree-title');
+      el.append($$('span').addClass('se-tree-node-name').append(node.name));
+    } else {
+      el.on('click', this._toggleFacet.bind(this, node.id));
+      el.append($$('span').addClass('se-tree-node-name').append(node.name));
+      el.append($$('span').addClass('se-tree-node-counter').append(node.count));
+    }
+
+    return concat(el, childrenEls);
+  };
+
+  this._expandNode = function(id, e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var rubrics = this.props.rubrics;
+    var isExpanded = rubrics.get([id, 'expanded']);
+    rubrics.set([id, 'expanded'], !isExpanded);
+    this.extendProps({
+      rubrics: rubrics
+    });
+  };
+
+  this._toggleFacet = function(id, e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var rubrics = this.props.rubrics;
+    var currentValue = rubrics.get([id, 'active']);
+    rubrics.set([id, 'active'], !currentValue);
+    this.extendProps({
+      rubrics: rubrics
+    });
+  };
+
+};
+
+Component.extend(Facets);
+
+module.exports = Facets;
