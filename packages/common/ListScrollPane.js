ec942b5b470c4980782d2da40890d7fd2e8f4dae
Daniel Beilinson <beilinson@gmail.com>
2016-07-12 21:23:18 +0300

    Scroll container not for documents.

---
+++ packages/common/ListScrollPane.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var ScrollPane = require('substance/ui/ScrollPane');
+
+function ListScrollPane() {
+  ScrollPane.apply(this, arguments);
+}
+
+ListScrollPane.Prototype = function() {
+  this.didMount = function() {
+    if (this.props.highlights) {
+      this.props.highlights.on('highlights:updated', this.onHighlightsUpdated, this);
+    }
+    // HACK: Scrollbar should use DOMMutationObserver instead
+    if (this.refs.scrollbar) {
+      //this.context.list.on('list:changed', this.onListChange, this, { priority: -1 });
+    }
+
+    this.handleActions({
+      'updateOverlayHints': this._updateOverlayHints
+    });
+  };
+
+  // HACK: Scrollbar should use DOMMutationObserver instead
+  this.onListChange = function() {
+    this.refs.scrollbar.updatePositions();
+  };
+};
+
+ScrollPane.extend(ListScrollPane);
+
+module.exports = ListScrollPane;
