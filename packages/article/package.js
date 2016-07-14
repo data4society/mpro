77170b75df17e6fee489d300cff6d843afbd0ab0
Daniel Beilinson <beilinson@gmail.com>
2016-07-14 16:21:51 +0300

    Article meta summary.

--- packages/article/package.js
+++ packages/article/package.js
@@ -14,6 +14,7 @@ var RubricatorPackage = require('../rubricator/package');
 var Article = require('../common/Article');
 var ArticleMeta = require('./ArticleMeta');
 var ArticleImporter = require('./ArticleImporter');
+var ArticleMetaComponent = require('./ArticleMetaComponent');
 
 module.exports = {
   name: 'article',
@@ -26,6 +27,22 @@ module.exports = {
 
     config.addNode(ArticleMeta);
     config.addImporter('html', ArticleImporter);
+
+    config.addComponent('meta-summary', ArticleMetaComponent);
+    config.addIcon('published', { 'fontawesome': 'fa-clock-o' });
+    config.addIcon('source', { 'fontawesome': 'fa-chain' });
+    config.addLabel('published-by', {
+      en: 'Published by',
+      ru: 'Опубликовано'
+    });
+    config.addLabel('published-date', {
+      en: ' on',
+      ru: ','
+    });
+    config.addLabel('source', {
+      en: 'Original source:',
+      ru: 'Источник:'
+    });
 
     config.import(BasePackage);
     config.import(ParagraphPackage);
