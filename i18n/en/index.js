969c8159db216e649c5d747a4fe8598aaaf5975c
Daniel Beilinson <beilinson@gmail.com>
2016-07-14 18:14:24 +0300

    Remove old i18n stuff.

--- i18n/en/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'sc-welcome.title': 'Monitoring Pro',
-  'sc-welcome.about': 'Monitoring Pro is a tool for search, attribution and extracting data from the incoming web-resources, as well as a tool for further analysis of this data.',
-  'sc-welcome.no-passwords': 'How to login?',
-  'sc-welcome.howto-login': 'Well, currently the only option is to click on <a href="/#loginKey=1234">this link</a>.',
-  'sc-welcome.enter-email': 'Later you\'ll be able to request login via next form or enter password.',
-  'sc-welcome.submit': 'Request login',
-  'meta.title': 'Title'
-};
