7f80a56319227f57d64ab83b5583a965bbb86585
Daniel Beilinson <beilinson@gmail.com>
2016-07-21 17:22:36 +0300

    Act entity model.

---
+++ packages/entities/Act.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var DocumentNode = require('substance/model/DocumentNode');
+
+function Act() {
+  Act.super.apply(this, arguments);
+}
+
+DocumentNode.extend(Act);
+
+Act.static.name = 'norm_act';
+
+/*
+  Entities Act node.
+  Holds Act entity.
+  
+  Attributes
+    - code Code name
+    - article Number of article
+    - content Content of article
+    - parent Parent article
+*/
+
+Act.static.defineSchema({
+  code: { type: 'string', default: '', field: { type: "select", options: ['УК', 'КоАП'], placeholder: "Pick a code" }},
+  article: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter article number" }},
+  content: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter article content" }},
+  parent: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter paretn article" }}
+});
+
+module.exports = Act;
