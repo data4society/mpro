3bada992c4cead13b52c219e1e71cb3a18ac206a
Daniel Beilinson <beilinson@gmail.com>
2016-07-21 17:24:14 +0300

    Connect forms and entities packages.

--- packages/mpro/package.js
+++ packages/mpro/package.js
@@ -16,18 +16,30 @@ var EditorPackage = require('../editor/package');
 var NotificationPackage = require('../notification/package');
 var CollaboratorsPackage = require('../collaborators/package');
 var WelcomePackage = require('../welcome/package');
+var FormsPackage = require('../forms/package.js');
 
+// Article configurator
+// Holds article schema
 var Article = require('../article/package');
 var articleViewerConfigurator = new SubConfigurator().import(ViewerPackage);
 articleViewerConfigurator.import(Article);
 
+// Vk configurator
+// Holds vkontakte posts schema
 var Vk = require('../vk/package');
 var vkViewerConfigurator = new SubConfigurator().import(ViewerPackage);
 vkViewerConfigurator.import(Vk);
 
+// Tng configurator
+// Holds training articles schema
 var Tng = require('../tng/package');
 var tngEditorConfigurator = new SubConfigurator().import(EditorPackage);
 tngEditorConfigurator.import(Tng);
+
+// Entities configurator
+// Holds entity container document schema
+var Entities = require('../entities/package');
+var entitiesConfigurator = new SubConfigurator().import(Entities);
 
 module.exports = {
   name: 'mpro',
@@ -45,10 +57,12 @@ module.exports = {
     config.import(NotificationPackage);
     config.import(CollaboratorsPackage);
     config.import(WelcomePackage);
+    config.import(FormsPackage);
     
     // Default configuration for available modes
     config.addConfigurator('viewer-mpro-article', articleViewerConfigurator);
     config.addConfigurator('viewer-mpro-vk', vkViewerConfigurator);
     config.addConfigurator('editor-mpro-tng', tngEditorConfigurator);
+    config.addConfigurator('mpro-entities', entitiesConfigurator);
   }
 };
