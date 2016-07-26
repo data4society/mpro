7e890a776ff12d6688c9c47e92fd348dcf851e49
Daniel Beilinson <beilinson@gmail.com>
2016-07-26 16:40:11 +0300

    Add more test seeds.

--- db/seed/entitySeed.sql
+++ db/seed/entitySeed.sql
@@ -5,4 +5,6 @@ values ('1', 'Martin Luther King, Jr', NOW(), NOW(), 'testuser', 'Person', '{"fi
 ('2', 'Jim Morrison', NOW(), NOW(), 'testuser', 'Person', '{"firstname": "Jim", "lastname": "Morisson", "occupation": ["musician", "poet"], "organization": "The Doors"}'),
 ('3', 'Amnesty International', NOW(), NOW(), 'testuser2', 'Organization', '{"type": "NGO", "founded": "1961", "location": "London"}'),
 ('4', 'Berlin Wall collapse', NOW(), NOW(), 'testuser2', 'Event', '{"date": "1989-11-09T21:00:00.000Z", "location": "Berlin"}'),
-('5', 'Jim Morrison', NOW(), NOW(), 'testuser', 'person', '{"firstname": "Jim", "lastname": "Morisson", "middlename": "", "nickname": "Lizard King", "status": "хэлпер", "position": ["Singer"], "role": ["Rock n Roll King"]}');
+('5', 'Jim Morrison', NOW(), NOW(), 'testuser', 'person', '{"firstname": "Jim", "lastname": "Morisson", "middlename": "", "nickname": "Lizard King", "status": "хэлпер", "position": ["Singer"], "role": ["Rock n Roll King"]}'),
+('6', '123 УК', NOW(), NOW(), 'testuser', 'norm_act', '{"code": "УК", "article": "12", "content": "Bla bla bla bla bla bla bla", "parent": ["7"]}'),
+('7', '1234 УК', NOW(), NOW(), 'testuser', 'norm_act', '{"code": "УК", "article": "10", "content": "Bla bla bla bla bla bla bla", "parent": null}');
