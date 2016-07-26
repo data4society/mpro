c3440d1f4abc16f34e614185f86a3379b4a1bb56
Daniel Beilinson <beilinson@gmail.com>
2016-07-26 16:42:06 +0300

    Use reference for selecting parent act.

--- packages/entities/Act.js
+++ packages/entities/Act.js
@@ -25,7 +25,7 @@ Act.static.defineSchema({
   code: { type: 'string', default: '', field: { type: "select", options: ['УК', 'КоАП'], placeholder: "Pick a code" }},
   article: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter article number" }},
   content: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter article content" }},
-  parent: { type: 'string', default: '', field: { type: "text", dataType: "text", placeholder: "Enter parent article" }}
+  parent: { type: 'string', default: '', field: { type: "reference", multiple: false, placeholder: "Enter parent article", restrictions: {"entity_class": "norm_act"}}}
 });
 
 module.exports = Act;
