e5fbca2c81059c15237d79491ff5a8790f4ffed4
Daniel Beilinson <beilinson@gmail.com>
2016-07-28 02:44:38 +0300

    Connect new tools and entity finder.

--- packages/entity/package.js
+++ packages/entity/package.js
@@ -1,10 +1,17 @@
 'use strict';
 
 var Entity = require('./Entity');
-var EntityTool = require('./EntityTool');
+var ActTool = require('./ActTool');
+var LocationTool = require('./LocationTool');
+var OrgTool = require('./OrgTool');
+var PersonTool = require('./PersonTool');
 var EditEntityTool = require('./EditEntityTool');
-var EntityCommand = require('./EntityCommand');
 var EntityComponent = require('./EntityComponent');
+var EntityCommand = require('./EntityCommand');
+var ActCommand = require('./ActCommand');
+var LocationCommand = require('./LocationCommand');
+var OrgCommand = require('./OrgCommand');
+var PersonCommand = require('./PersonCommand');
 var FormsPackage = require('../forms/package.js');
 
 module.exports = {
@@ -12,12 +19,23 @@ module.exports = {
   configure: function(config) {
     config.addNode(Entity);
     config.addComponent(Entity.static.name, EntityComponent);
-    config.addTool(EntityTool, {viewer: true});
+    config.addTool(ActTool, {viewer: true});
+    config.addTool(LocationTool, {viewer: true});
+    config.addTool(OrgTool, {viewer: true});
+    config.addTool(PersonTool, {viewer: true});
     config.addTool(EditEntityTool, {overlay: true});
     config.addCommand(EntityCommand);
-    config.addIcon(EntityCommand.static.name, { 'fontawesome': 'fa-users' });
+    config.addCommand(ActCommand);
+    config.addCommand(LocationCommand);
+    config.addCommand(OrgCommand);
+    config.addCommand(PersonCommand);
+    config.addIcon(ActCommand.static.name, { 'fontawesome': 'fa-balance-scale' });
+    config.addIcon(LocationCommand.static.name, { 'fontawesome': 'fa-globe' });
+    config.addIcon(OrgCommand.static.name, { 'fontawesome': 'fa-building' });
+    config.addIcon(PersonCommand.static.name, { 'fontawesome': 'fa-users' });
     config.addIcon('delete', { 'fontawesome': 'fa-trash' });
     config.addStyle(__dirname, '_entity');
+    config.addStyle(__dirname, '_entity-finder');
     config.addLabel('entity', {
       en: 'Entity reference',
       ru: 'Упоминание сущности'
