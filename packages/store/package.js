22916a627deecf329c8d60fa5e773ada826caca7
Daniel Beilinson <beilinson@gmail.com>
2016-10-15 16:07:12 +0300

    Store package definition.

---
+++ packages/store/package.js
@@ -0,0 +1,30 @@
+let ChangeStore = require('./ChangeStore')
+let ClassStore = require('./ClassStore')
+let DocumentStore = require('./DocumentStore')
+let EntityStore = require('./EntityStore')
+let FileStore = require('./FileStore')
+let MarkupStore = require('./MarkupStore')
+let ReferenceStore = require('./ReferenceStore')
+let RubricStore = require('./RubricStore')
+let SessionStore = require('./SessionStore')
+let SnapshotStore = require('./SnapshotStore')
+let SourceStore = require('./SourceStore')
+let UserStore = require('./UserStore')
+
+module.exports = {
+  name: 'mpro-store',
+  configure: function(config) {
+    config.addStore('change', ChangeStore)
+    config.addStore('class', ClassStore)
+    config.addStore('document', DocumentStore)
+    config.addStore('entity', EntityStore)
+    config.addStore('file', FileStore)
+    config.addStore('markup', MarkupStore)
+    config.addStore('reference', ReferenceStore)
+    config.addStore('rubric', RubricStore)
+    config.addStore('session', SessionStore)
+    config.addStore('snapshot', SnapshotStore)
+    config.addStore('source', SourceStore)
+    config.addStore('user', UserStore)
+  }
+}
