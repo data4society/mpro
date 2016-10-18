d4ffd17e535d871bf0f622403dd6a1a1c445e671
Daniel Beilinson <beilinson@gmail.com>
2016-10-18 15:30:06 +0300

    Fix memory heap issue.

--- packages/server/CollabServer.js
+++ packages/server/CollabServer.js
@@ -3,6 +3,7 @@ let DocumentChange = require('substance').DocumentChange
 let Err = require('substance').SubstanceError
 let isUndefined = require('lodash/isUndefined')
 let extend = require('lodash/extend')
+let find = require('lodash/find')
 
 /*
   DocumentServer module. Can be bound to an express instance
@@ -39,57 +40,61 @@ class MproCollabServer extends CollabServer {
     if (message.type === 'sync') {
       // We fetch the document record to get the old title
       this.documentStore.getDocument(message.documentId, function(err, docRecord) {
-        if(err) return this.next(err)
+        if(err) {
+          console.error('enhanceRequest returned an error', err)
+          this._error(req, res, err)
+          return
+        }
 
-        var updatedAt = new Date();
-        var title = docRecord.title;
-        var meta = docRecord.content ? find(docRecord.content.nodes, { 'id': 'meta'}) : docRecord.meta;
-        var rubrics = meta.rubrics;
-        var entities = meta.entities;
-        var accepted;
+        let updatedAt = new Date()
+        let title = docRecord.title
+        let meta = docRecord.content ? find(docRecord.content.nodes, { 'id': 'meta'}) : docRecord.meta
+        let rubrics = meta.rubrics
+        let entities = meta.entities
+        let accepted
 
         if (message.change) {
           // Update the title if necessary
-          var change = DocumentChange.fromJSON(message.change);
+          let change = DocumentChange.fromJSON(message.change)
           change.ops.forEach(function(op) {
             if(op.path[0] === 'meta' && op.path[1] === 'title') {
-              title = op.diff.apply(title);
+              title = op.diff.apply(title)
             }
-          });
+          })
 
           change.ops.forEach(function(op) {
             if(op.path[0] === 'meta' && op.path[1] === 'rubrics') {
-              rubrics = op.val;
+              rubrics = op.val
             }
-          });
+          })
 
           change.ops.forEach(function(op) {
             if(op.path[0] === 'meta' && op.path[1] === 'entities') {
-              entities = op.val;
+              entities = op.val
             }
-          });
+          })
 
           change.ops.forEach(function(op) {
             if(op.path[0] === 'meta' && op.path[1] === 'accepted') {
-              accepted = op.val;
+              accepted = op.val
             }
-          });
+          })
 
           message.change.info = {
             userId: req.session.userId,
             updatedAt: updatedAt
-          };
+          }
         }
 
         message.collaboratorInfo = {
           name: req.session.user.name
-        };
+        }
 
         // update meta object with modified properties
-        extend(meta, {title: title, rubrics: rubrics, entities: entities});
+        extend(meta, {title: title, rubrics: rubrics, entities: entities})
 
         if(!isUndefined(accepted)) {
-          extend(meta, {accepted: accepted});
+          extend(meta, {accepted: accepted})
         }
 
         // commit and connect method take optional documentInfo argument
@@ -99,9 +104,10 @@ class MproCollabServer extends CollabServer {
           title: title,
           meta: meta,
           rubrics: rubrics
-        };
+        }
+        req.setEnhanced()
         this.next(req, res)
-      }.bind(this));
+      }.bind(this))
     } else {
       // Just continue for everything that is not handled
       req.setEnhanced()
