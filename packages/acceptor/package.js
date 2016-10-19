727a5c7aa2ba4823b373324b637e5f09b0fcfa6c
Daniel Beilinson <beilinson@gmail.com>
2016-10-19 18:21:49 +0300

    Acceptor cleanup.

--- packages/acceptor/package.js
+++ packages/acceptor/package.js
@@ -4,7 +4,7 @@ import AcceptorCommand from './AcceptorCommand'
 export default {
   name: 'acceptor',
   configure: function(config) {
-    config.addTool('acceptor', AcceptorTool, {editor: true})
+    config.addTool('acceptor', AcceptorTool)
     config.addCommand('acceptor', AcceptorCommand)
     config.addIcon('acceptor', { 'fontawesome': 'fa-check' })
   }
