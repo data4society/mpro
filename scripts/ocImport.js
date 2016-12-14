def3a60832cecdcc06d40128b797c4e48d2528ce
Daniel Beilinson <beilinson@gmail.com>
2016-12-15 02:00:03 +0300

    OC import script.

---
+++ scripts/ocImport.js
@@ -0,0 +1,21 @@
+let Database = require('../packages/common/Database')
+let Configurator = require('../packages/common/ServerConfigurator')
+let EnginePackage = require('../packages/engine/package')
+
+let fileName = process.argv[2]
+let db = new Database()
+let configurator = new Configurator()
+configurator.setDBConnection(db)
+configurator.import(EnginePackage)
+
+let importEngine = configurator.getEngine('import')
+
+importEngine.openCorporaImporter(fileName)
+  .then(function() {
+    console.log('Import has been finished')
+    db.shutdown()
+  })
+  .catch(function(err) {
+    console.log(err)
+    db.shutdown()
+  })
