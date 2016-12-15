fa3e6cf5925747c6bb69e23774a3641065732111
Daniel Beilinson <beilinson@gmail.com>
2016-12-16 00:25:24 +0300

    Entity class filter.

--- packages/resources/Filters.js
+++ packages/resources/Filters.js
@@ -1,11 +1,16 @@
-import { Component } from 'substance'
+import { Component, SubstanceError as Err } from 'substance'
 import InputFilter from './InputFilter'
+import SelectFilter from './SelectFilter'
 
 class Filters extends Component {
 
+  didMount() {
+    this._loadFilterValues()
+  }
+
   getInitialState() {
     return {
-      selects: [],
+      selects: ['entity_class'],
       filterOptions: {}
     }
   }
@@ -13,15 +18,42 @@ class Filters extends Component {
   render($$) {
     let el = $$('div').addClass('sc-filters')
 
-    let quick_find = $$('div').addClass('se-quick-find').append(
+    let quickFind = $$('div').addClass('se-quick-find').append(
       $$(InputFilter, {name: 'name', op: '~~*', type: 'text', placeholder: 'Enter resource name', filter: this.props.name})
     )
 
+    let entityClassSelector = $$('div').addClass('sc-field-select').append(
+      $$(SelectFilter, {name: 'entity_class', placeholder: 'Entity class', options: this.state.filterOptions['entity_class'], filter: this.props['entity_class']})
+    )
+
     el.append(
-      quick_find
+      quickFind,
+      entityClassSelector
     )
 
     return el
+  }
+
+  _loadFilterValues() {
+    let documentClient = this.context.documentClient
+    let selects = this.state.selects
+
+    documentClient.getFilterValues(selects, 'entities', (err, options) => {
+      if (err) {
+        this.setState({
+          error: new Err('Filters.LoadingError', {
+            message: 'Filters could not be loaded.',
+            cause: err
+          })
+        })
+        console.error('ERROR', err)
+        return
+      }
+
+      this.extendState({
+        filterOptions: options
+      })
+    })
   }
 }
 
