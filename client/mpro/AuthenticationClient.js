9240ac13da66b6dd4b4c60eef45fc97b11268314
Daniel Beilinson <beilinson@gmail.com>
2016-12-21 17:56:19 +0300

    Fix change name issue.

--- client/mpro/AuthenticationClient.js
+++ client/mpro/AuthenticationClient.js
@@ -10,6 +10,28 @@ class AuthenticationClient {
     this._requests = {};
   }
 
+  request(method, url, data, cb) {
+    let request = new XMLHttpRequest();
+    request.open(method, url, true)
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+    request.setRequestHeader('x-access-token', this.getSessionToken())
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        let res = request.responseText;
+        if(isJson(res)) res = JSON.parse(res);
+        cb(null, res);
+      } else {
+        return cb(new Error('Request failed. Returned status: ' + request.status))
+      }
+    }
+
+    if (data) {
+      request.send(JSON.stringify(data))
+    } else {
+      request.send()
+    }
+  }
+
   getSession() {
     return this._session
   }
@@ -30,7 +52,7 @@ class AuthenticationClient {
     this._requests['changeName'] = userId + name
 
     let path = this.config.httpUrl + 'changename'
-    request('POST', path, {
+    this.request('POST', path, {
       userId: userId,
       name: name
     }, function(err, res) {
@@ -97,6 +119,15 @@ class AuthenticationClient {
     }.bind(this))
   }
 
+}
+
+function isJson(str) {
+  try {
+    JSON.parse(str)
+  } catch (e) {
+    return false
+  }
+  return true
 }
 
 export default AuthenticationClient
