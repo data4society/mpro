b4f77657643c7a8d079a08d4ed1cad31b07a1850
Daniel Beilinson <beilinson@gmail.com>
2016-12-28 00:58:08 +0300

    Print source inside tng meta.

--- packages/tng/TngMetaComponent.js
+++ packages/tng/TngMetaComponent.js
@@ -3,7 +3,24 @@ import { Component } from 'substance'
 class TngMetaComponent extends Component {
 
   render($$) {
+    let document = this.context.doc
+    let meta = document.get('meta')
     let el = $$('div').addClass('sc-meta-summary')
+    let source = $$('div').addClass('se-source').append(
+      this.context.iconProvider.renderIcon($$, 'source'),
+      $$('div').addClass('sm-item').append(
+        this.getLabel('source') + ' ',
+        $$('a').addClass('se-source-url')
+          .setAttribute('href', meta.source)
+          .setAttribute('target', '_blank')
+          .append(
+            meta.source
+          )
+      )
+    )
+
+    el.append(source)
+
     return el
   }
 }
