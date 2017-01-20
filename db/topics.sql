c48fec878309a02a5ccaf3e3f57f63c0687a07e3
Daniel Beilinson <beilinson@gmail.com>
2017-01-20 18:02:59 +0300

    Remove topics example.

--- db/topics.sql
+++
@@ -1,6 +0,0 @@
-SELECT * from
-(SELECT DISTINCT ON (theme_id) *,
-(SELECT COUNT(*) FROM themed_records_materialized_view a WHERE a.theme_id = t.theme_id) AS count
-FROM themed_records_materialized_view t
-ORDER BY theme_id, created DESC LIMIT 30) as foo
-ORDER BY created DESC;
