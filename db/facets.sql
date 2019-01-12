d93bb77a13db94d7f8cb057c4c226838792057c0
Daniel <beilinson@gmail.com>
2019-01-12 14:46:59 +0300

    Cast uuid type.

--- db/facets.sql
+++ db/facets.sql
@@ -3,4 +3,4 @@ SELECT rubric, cnt, rubrics.name FROM (
     unnest(records.rubrics) AS rubric,
     COUNT(*) OVER (PARTITION BY unnest(records.rubrics)) cnt
   FROM records WHERE $1 <@ rubrics AND training = $2
-) AS docs INNER JOIN rubrics ON (docs.rubric = rubrics.rubric_id);
+) AS docs INNER JOIN rubrics ON (docs.rubric = rubrics.rubric_id::text);
